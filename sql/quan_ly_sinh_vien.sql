8556fc21285ad6ee5182bc8a79cc2cd3d86a2ee3
HaiPV <mitto.hai.7356@gmail.com>
2021-07-04 03:54:52 +0700

    sql

--- sql/quan_ly_sinh_vien.sql
+++ sql/quan_ly_sinh_vien.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3308
--- Generation Time: Jun 30, 2021 at 07:17 PM
+-- Generation Time: Jul 03, 2021 at 08:54 PM
 -- Server version: 5.7.28
 -- PHP Version: 7.3.12
 
@@ -189,91 +189,20 @@ CREATE TABLE IF NOT EXISTS `hoa_don` (
   `ma_ma_the` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `created_at` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=79 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=116 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `hoa_don`
 --
 
 INSERT INTO `hoa_don` (`id`, `ma_the_nap`, `user_tao_hd`, `status`, `ma_giao_dich`, `ma_ma_the`, `created_at`) VALUES
-(1, 1, 17, 1, NULL, NULL, 1625078295),
-(2, 2, 17, 1, NULL, NULL, 1625078450),
-(3, 2, 17, 1, NULL, NULL, 1625078513),
-(4, 2, 17, 1, NULL, NULL, 1625078515),
-(5, 2, 17, 1, NULL, NULL, 1625078515),
-(6, 2, 17, 1, NULL, NULL, 1625078515),
-(7, 2, 17, 1, NULL, NULL, 1625078515),
-(8, 2, 17, 1, NULL, NULL, 1625078515),
-(9, 2, 17, 1, NULL, NULL, 1625078516),
-(10, 2, 17, 1, NULL, NULL, 1625078516),
-(11, 2, 17, 1, NULL, NULL, 1625078516),
-(12, 2, 17, 1, NULL, NULL, 1625078530),
-(13, 2, 17, 1, NULL, NULL, 1625078535),
-(14, 2, 17, 1, NULL, NULL, 1625078535),
-(15, 2, 17, 1, NULL, NULL, 1625078535),
-(16, 2, 17, 1, NULL, NULL, 1625078535),
-(17, 2, 17, 1, NULL, NULL, 1625078545),
-(18, 2, 17, 1, NULL, NULL, 1625078545),
-(19, 2, 17, 1, NULL, NULL, 1625078545),
-(20, 2, 17, 1, NULL, NULL, 1625078665),
-(21, 2, 17, 1, NULL, NULL, 1625078665),
-(22, 2, 17, 1, NULL, NULL, 1625078665),
-(23, 2, 17, 1, NULL, NULL, 1625078665),
-(24, 2, 17, 1, NULL, NULL, 1625078704),
-(25, 2, 17, 1, NULL, NULL, 1625078705),
-(26, 2, 17, 1, NULL, NULL, 1625078705),
-(27, 2, 17, 1, NULL, NULL, 1625078706),
-(28, 2, 17, 1, NULL, NULL, 1625078706),
-(29, 2, 17, 1, NULL, NULL, 1625078706),
-(30, 2, 17, 1, NULL, NULL, 1625078709),
-(31, 2, 17, 1, NULL, NULL, 1625078726),
-(32, 2, 17, 1, NULL, NULL, 1625078735),
-(33, 2, 17, 1, NULL, NULL, 1625078736),
-(34, 2, 17, 1, NULL, NULL, 1625078736),
-(35, 2, 17, 1, NULL, NULL, 1625078749),
-(36, 2, 17, 1, NULL, NULL, 1625078750),
-(37, 2, 17, 1, NULL, NULL, 1625078750),
-(38, 2, 17, 1, NULL, NULL, 1625078756),
-(39, 2, 17, 1, NULL, NULL, 1625078756),
-(40, 2, 17, 1, NULL, NULL, 1625078809),
-(41, 2, 17, 1, NULL, NULL, 1625078810),
-(42, 2, 17, 1, NULL, NULL, 1625078810),
-(43, 2, 17, 1, NULL, NULL, 1625078810),
-(44, 2, 17, 1, NULL, NULL, 1625078810),
-(45, 2, 17, 1, NULL, NULL, 1625078821),
-(46, 2, 17, 1, NULL, NULL, 1625079131),
-(47, 2, 17, 1, NULL, NULL, 1625079133),
-(48, 2, 17, 1, NULL, NULL, 1625079134),
-(49, 2, 17, 1, NULL, NULL, 1625079134),
-(50, 2, 17, 1, NULL, NULL, 1625079134),
-(51, 2, 17, 1, NULL, NULL, 1625079134),
-(52, 2, 17, 1, NULL, NULL, 1625079135),
-(53, 2, 17, 1, NULL, NULL, 1625079135),
-(54, 2, 17, 1, NULL, NULL, 1625079135),
-(55, 2, 17, 1, NULL, NULL, 1625079136),
-(56, 2, 17, 1, NULL, NULL, 1625079136),
-(57, 2, 17, 1, NULL, NULL, 1625079136),
-(58, 2, 17, 1, NULL, NULL, 1625079136),
-(59, 2, 17, 1, NULL, NULL, 1625079251),
-(60, 2, 17, 1, NULL, NULL, 1625079252),
-(61, 2, 17, 1, NULL, NULL, 1625079252),
-(62, 2, 17, 1, NULL, NULL, 1625079252),
-(63, 2, 17, 1, NULL, NULL, 1625079252),
-(64, 2, 17, 1, NULL, NULL, 1625079253),
-(65, 2, 17, 1, NULL, NULL, 1625079253),
-(66, 2, 17, 1, NULL, NULL, 1625079254),
-(67, 2, 17, 1, NULL, NULL, 1625079272),
-(68, 2, 17, 1, NULL, NULL, 1625079280),
-(69, 2, 17, 1, NULL, NULL, 1625079281),
-(70, 2, 17, 1, NULL, NULL, 1625079291),
-(71, 2, 17, 1, NULL, NULL, 1625079326),
-(72, 2, 17, 1, NULL, NULL, 1625079374),
-(73, 2, 17, 1, NULL, NULL, 1625080457),
-(74, 2, 17, 1, NULL, NULL, 1625080457),
-(75, 2, 17, 1, NULL, NULL, 1625080457),
-(76, 2, 17, 1, NULL, NULL, 1625080564),
-(77, 2, 17, 1, NULL, NULL, 1625080596),
-(78, 2, 17, 1, NULL, NULL, 1625080614);
+(109, 1, 17, 1, NULL, NULL, 1625339309),
+(110, 1, 17, 2, NULL, '1', 1625339657),
+(111, 1, 17, 2, NULL, '2', 1625339767),
+(112, 1, 17, 2, NULL, '3', 1625343347),
+(113, 2, 17, 2, NULL, '4', 1625344835),
+(114, 1, 17, 2, NULL, '5', 1625344938),
+(115, 1, 17, 2, NULL, '6', 1625345190);
 
 -- --------------------------------------------------------
 
@@ -425,7 +354,19 @@ CREATE TABLE IF NOT EXISTS `ma_the` (
   `user_su_dung` int(11) DEFAULT NULL,
   `created_at` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `ma_the`
+--
+
+INSERT INTO `ma_the` (`id`, `ma_the`, `ma_the_nap`, `trang_thai`, `tg_nap`, `user_su_dung`, `created_at`) VALUES
+(1, 'iNT2b9mDA7wu7icCpK8bH', 1, 0, NULL, NULL, 1625339691),
+(2, 'gkIzyPu5QKKCoYZrOu0PT', 1, 0, NULL, NULL, 1625339795),
+(3, 'G7d7ZLBvLGuMev13Iy7zm', 1, 0, NULL, NULL, 1625343454),
+(4, '2HvZu4w3BXvs2yFe6WDAA', 2, 0, NULL, NULL, 1625344872),
+(5, 'y1VBZRzExhnKIJCmMEeSE', 1, 0, NULL, NULL, 1625345074),
+(6, 'kv5ypX7MRMxWF1WWzwpNF', 1, 0, NULL, NULL, 1625345463);
 
 -- --------------------------------------------------------
 
@@ -1430,10 +1371,10 @@ CREATE TABLE IF NOT EXISTS `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `email`, `info_id`, `password_hash`, `auth_key`, `confirmed_at`, `unconfirmed_email`, `blocked_at`, `registration_ip`, `created_at`, `updated_at`, `flags`, `last_login_at`) VALUES
-(1, 'admin', 'mitto.hai.7356@gmail.com6', 0, '$2y$10$L0xLefcDfQHiS4d3wg/CE.dPmq6IQq5lYgCY4SJv0IDdcYRe/GDkO', 'cazH3uZa4-qu3--HMNwJk92DB024IJ0k', 1622739891, NULL, NULL, '::1', 1622739891, 1622739891, 0, 1625074279),
+(1, 'admin', 'mitto.hai.7356@gmail.com6', 0, '$2y$10$L0xLefcDfQHiS4d3wg/CE.dPmq6IQq5lYgCY4SJv0IDdcYRe/GDkO', 'cazH3uZa4-qu3--HMNwJk92DB024IJ0k', 1622739891, NULL, NULL, '::1', 1622739891, 1622739891, 0, 1625323721),
 (15, 'phantranbaotuan', 'phantranbaotuan@gmail.com', 2, '$2y$10$puczsqcNubiq0jk6U9DBn.j26WRCZCO5oJfZRTsPAxjcsLizkkKcy', '2zAR6eHhUOmfDs6MtUScOBTVun7xT4iL', 1623613690, NULL, NULL, '::1', 1623613690, 1623613690, 1, 1624473707),
 (16, 'nguyenthiennam', 'nguyenthiennam@gmail.com', 1, '$2y$10$gTTEjicwX8tAFFEKFhAYhOvmoFfI/.3aFwmZw4WXKOq699C53w7b2', 'LGnvC_h1aMQlatLrCIv4hdD6Qr2AIw4M', 1623613703, NULL, NULL, '::1', 1623613704, 1623613704, 1, 1624473267),
-(17, 'tranduybao', 'tranduybao@gmail.com', 3, '$2y$10$MY.a2H9tY9HyM72R5tUx3uiSYEmxI9BCk7mu0kmVTvfKrES4SxgTK', 'gyHYeklNBXQGE0ZYomttINrfDexgMgNk', 1623696009, NULL, NULL, '::1', 1623696009, 1623696009, 1, 1625074393);
+(17, 'tranduybao', 'tranduybao@gmail.com', 3, '$2y$10$MY.a2H9tY9HyM72R5tUx3uiSYEmxI9BCk7mu0kmVTvfKrES4SxgTK', 'gyHYeklNBXQGE0ZYomttINrfDexgMgNk', 1623696009, NULL, NULL, '::1', 1623696009, 1623696009, 1, 1625323733);
 
 --
 -- Constraints for dumped tables
