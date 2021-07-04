f2356964f8ec7e694d8947d8da20139543b0e43e
HaiPV <mitto.hai.7356@gmail.com>
2021-07-04 13:47:25 +0700

    todo

--- common/config/params.php
+++ common/config/params.php
@@ -1,15 +1,7 @@
 <?php
-//Thẻ ABB:
-//Số thẻ: 9704250000000001
-//Tháng/Năm phát hành: 01/13
-//Tên: NGUYEN VAN A
-//Mã OTP: 123456
 return [
 	'format'=>[
 		'date'=> 'php:l, Y-m-d H:i:s',
 	],
 	'date'=>'l, Y-m-d H:i:s',
-	'onepay'=>[
-		'ReturnURL' => 'http://sinhvien.hou.demo/dich-vu/checkout.html'
-	]
 ];
